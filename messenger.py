06c992598a64d0517d188495247cd134ee6a8f8f
fleshmanj <fleshmanjoshuac@hotmail.com>
2021-12-21 14:38:49 -0600

    Working on getting the input buffer to print to lcd properly

--- messenger.py
+++ messenger.py
@@ -9,18 +9,22 @@ import keyboard  # using module keyboard
 class Messenger(object):
     key = keyboard
     states = ["main_menu", "send_menu", "received_menu", "settings_menu", "set_address", "set_networkid",
-              "set_parameters", "compose_message"]
+              "set_parameters", "compose_message", "sender_list"]
 
     def __init__(self, lcd_to_use, row=0, col=0):
         self.lcd = lcd_to_use
+        self.lcd.home = (0, 0)
         self.row = row
         self.col = col
         self.buffer = ""
         self.menus = {"main menu": ("* Send", "* Messages", "* Settings"),
-                      "send menu": ("To:", "Compose", "                   ", "Main menu"),
-                      "received menu": ("Sender", "                    ", "                    ", "Main menu"),
-                      "settings menu": ("* Set addr (0-65535)", "* Set ntwk (0-16)", "                    ", "Main menu"),
-                      "compose message": ("                    ", "                    ", "                    ", "Main menu")}
+                      "send menu": ("To:", "Compose", "", "Main menu"),
+                      "received menu": ("Sender", "", "", "Main menu"),
+                      "settings menu": ("* Set addr (0-65535)", "* Set ntwk (0-16)", "", "Main menu"),
+                      "compose message": ("", "", "", "Main menu"),
+                      "sender list": ("", "", "", "Main menu")}
+
+        self.input_buffer = ""
         # Initialize the state machine
 
         self.machine = Machine(model=self, states=Messenger.states, initial='main_menu')
@@ -37,27 +41,35 @@ class Messenger(object):
         self.machine.add_transition('set_network', 'settings_menu', 'set_networkid')
         self.machine.add_transition('set_params', 'settings_menu', 'set_parameters')
         self.machine.add_transition('main_menu', '*', 'main_menu')
-        self.machine.add_transition('compose', 'send_menu', 'compose_message')
+        self.machine.add_transition('compose_message', 'send_menu', 'compose_message')
+        self.machine.add_transition('sender_list', 'received_menu', 'sender_list')
 
     def update_screen(self):
         self.lcd.clear()
         state = self.state
         print(state)
+        self.print_input_buffer()
         if state == "main_menu":
-            self.print_main_menu(self.menus["main menu"])
+            self.print_menu(self.menus["main menu"])
         if state == "send_menu":
-            self.print_main_menu(self.menus["send menu"])
+            self.print_menu(self.menus["send menu"])
         if state == "received_menu":
-            self.print_main_menu(self.menus["received menu"])
+            self.print_menu(self.menus["received menu"])
         if state == "settings_menu":
-            self.print_main_menu(self.menus["settings menu"])
+            self.print_menu(self.menus["settings menu"])
         if state == "compose_message":
-            self.print_main_menu(self.menus["compose message"])
+            self.print_menu(self.menus["compose message"])
+        if state == "sender_list":
+            self.print_menu(self.menus["sender list"])
+
+
 
         self.lcd.set_cursor_pos(self.row, self.col)
         self.lcd.draw_cursor()
+        print("update screen")
 
-    def print_main_menu(self, strings):
+
+    def print_menu(self, strings):
         # strings = ("* Send", "* Messages", "* Settings")
         row = 0
         for item in strings:
@@ -65,29 +77,34 @@ class Messenger(object):
             self.lcd.print(item)
             row += 1
 
-    def print_send_menu(self):
-        strings = ("To:", "Compose", "                   ", "Main menu")
-        row = 0
-        for item in strings:
-            self.lcd.set_cursor_pos(row, 0)
-            self.lcd.print(item)
-            row += 1
 
-    def print_received_menu(self):
-        strings = ("Sender", "                    ", "                    ", "Main menu")
+    def print_input_buffer(self):
         row = 0
-        for item in strings:
-            self.lcd.set_cursor_pos(row, 0)
-            self.lcd.print(item)
-            row += 1
+        col = 0
 
-    def print_settings_menu(self):
-        strings = ("* Set addr (0-65535)", "* Set ntwk (0-16)", "                    ", "Main menu")
-        row = 0
-        for item in strings:
-            self.lcd.set_cursor_pos(row, 0)
-            self.lcd.print(item)
-            row += 1
+        for char in self.input_buffer:
+            self.lcd.set_cursor_pos(0, col)
+
+            if col < self.lcd.width - 1:
+                self.lcd.print(char)
+                col += 1
+                self.lcd.set_cursor_pos(row, col)
+            elif row == self.lcd.height - 2 and col == self.lcd.width - 1:
+                pass
+            else:
+                row += 1
+                col = 0
+                self.lcd.print(char)
+                self.lcd.set_cursor_pos(row, col)
+
+            self.row = row
+            self.col = col
+        self.lcd.set_cursor_pos(self.row, self.col)
+
+
+
+
+
 
     def on_up(self):
         if self.row > 0:
@@ -101,8 +118,6 @@ class Messenger(object):
         if self.col > 0:
             self.col -= 1
             self.lcd.set_cursor_pos(self.row, self.col)
-            print("Pressing a!")
-            print(str(self.row) + ", " + str(self.col))
         self.update_screen()
 
     def on_down(self):
@@ -125,13 +140,9 @@ class Messenger(object):
         print("Pressing p!")
         print(self.lcd.current_cursor_pos())
         print(self.state)
-        buffer = []
+        buffer = self.input_buffer
 
-        for x in range(4):
-            for y in range(20):
-                # buffer.append(self.lcd.get_char(y,x)) = "".join(buffer, chr(self.lcd.window.getch([y,x])))
-                print(chr(curses.window.inch(x,y)))
-        # print(buffer)
+        print(buffer)
 
     def on_enter(self):
         print("Pressing enter")
@@ -152,7 +163,7 @@ class Messenger(object):
                 self.col = 0
         elif state == "send_menu":
             if self.row == 1 and self.col == 0:
-                self.compose()
+                self.compose_message()
                 self.row = 0
                 self.col = 0
             if self.row == 3 and self.col == 0:
@@ -165,6 +176,15 @@ class Messenger(object):
                 self.row = 0
                 self.col = 0
         elif state == "received_menu":
+            if self.row == 0 and self.col == 0:
+                self.sender_list()
+                self.row = 0
+                self.col = 0
+            if self.row == 3 and self.col == 0:
+                self.main_menu()
+                self.row = 0
+                self.col = 0
+        elif state == "sender_list":
             if self.row == 3 and self.col == 0:
                 self.main_menu()
                 self.row = 0
@@ -178,26 +198,49 @@ class Messenger(object):
         self.update_screen()
 
     def write_char(self, st):
-        if self.col < self.lcd.width - 1:
-            self.lcd.print(st)
-            self.col = self.col + 1
-            self.lcd.set_cursor_pos(self.row, self.col)
-        elif self.row == self.lcd.height - 2 and self.col == self.lcd.width - 1:
-            pass
-        else:
-            self.row = self.row + 1
-            self.col = 0
-            self.lcd.print(st)
-            self.lcd.set_cursor_pos(self.row, self.col)
+        self.input_buffer += st
+        self.update_screen()
+        print(" write char")
 
-    def space(self):
-        self.lcd.print(" ")
-        self.col = self.col + 1
-        self.lcd.set_cursor_pos(self.row, self.col)
+
+
+
+    def print_char(self, st):
+        state = self.state
+        if state == "compose_message":
+            if self.col < self.lcd.width - 1:
+                self.lcd.print(st)
+                self.col += 1
+                self.lcd.set_cursor_pos(self.row, self.col)
+            elif self.row == self.lcd.height - 2 and self.col == self.lcd.width - 1:
+                pass
+            else:
+                self.row = self.row + 1
+                self.col = 0
+                self.lcd.print(st)
+                self.lcd.set_cursor_pos(self.row, self.col)
+
+        elif state == "send_message":
+            if self.col < self.lcd.width - 1:
+                self.lcd.print(st)
+                self.col = self.col + 1
+                self.lcd.set_cursor_pos(self.row, self.col)
+            elif self.row == self.lcd.height - 2 and self.col == self.lcd.width - 1:
+                pass
+            else:
+                self.row = self.row + 1
+                self.col = 0
+                self.lcd.set_cursor_pos(self.row, self.col)
+        else:
+            pass
+
 
     def delete(self):
         if self.col > 0:
             self.lcd.delete(self.col, self.row)
+            delete_chr = " "
+            self.input_buffer = self.input_buffer[:len(self.input_buffer)-1]
+            # self.input_buffer = self.input_buffer[]
             self.col = self.col - 1
             print("this works")
             print(self.col)
@@ -231,34 +274,34 @@ if __name__ == '__main__':
     key.add_hotkey("d+right_shift", main.on_right)
     key.add_hotkey("p+right_shift", main.on_p)
     key.add_hotkey("enter", main.on_enter)
-    key.add_hotkey("space", main.space)
+    key.add_hotkey("space", main.write_char, args=[" "], suppress=True)
     key.add_hotkey("backspace", main.delete)
-    key.add_hotkey("q", main.write_char, args=["q"])
-    key.add_hotkey("w", main.write_char, args=["w"])
-    key.add_hotkey("e", main.write_char, args=["e"])
-    key.add_hotkey("r", main.write_char, args=["r"])
-    key.add_hotkey("t", main.write_char, args=["t"])
-    key.add_hotkey("y", main.write_char, args=["y"])
-    key.add_hotkey("u", main.write_char, args=["u"])
-    key.add_hotkey("i", main.write_char, args=["i"])
-    key.add_hotkey("o", main.write_char, args=["o"])
-    key.add_hotkey("p", main.write_char, args=["p"])
-    key.add_hotkey("a", main.write_char, args=["a"])
-    key.add_hotkey("s", main.write_char, args=["s"])
-    key.add_hotkey("d", main.write_char, args=["d"])
-    key.add_hotkey("f", main.write_char, args=["f"])
-    key.add_hotkey("g", main.write_char, args=["g"])
-    key.add_hotkey("h", main.write_char, args=["h"])
-    key.add_hotkey("j", main.write_char, args=["j"])
-    key.add_hotkey("k", main.write_char, args=["k"])
-    key.add_hotkey("l", main.write_char, args=["l"])
-    key.add_hotkey("z", main.write_char, args=["z"])
-    key.add_hotkey("x", main.write_char, args=["x"])
-    key.add_hotkey("c", main.write_char, args=["c"])
-    key.add_hotkey("v", main.write_char, args=["v"])
-    key.add_hotkey("b", main.write_char, args=["b"])
-    key.add_hotkey("n", main.write_char, args=["n"])
-    key.add_hotkey("m", main.write_char, args=["m"])
+    key.add_hotkey("q", main.print_char, args=["q"], suppress=True)
+    key.add_hotkey("w", main.write_char, args=["w"], suppress=True)
+    key.add_hotkey("e", main.write_char, args=["e"], suppress=True)
+    key.add_hotkey("r", main.write_char, args=["r"], suppress=True)
+    key.add_hotkey("t", main.write_char, args=["t"], suppress=True)
+    key.add_hotkey("y", main.write_char, args=["y"], suppress=True)
+    key.add_hotkey("u", main.write_char, args=["u"], suppress=True)
+    key.add_hotkey("i", main.write_char, args=["i"], suppress=True)
+    key.add_hotkey("o", main.write_char, args=["o"], suppress=True)
+    key.add_hotkey("p", main.write_char, args=["p"], suppress=True)
+    key.add_hotkey("a", main.write_char, args=["a"], suppress=True)
+    key.add_hotkey("s", main.write_char, args=["s"], suppress=True)
+    key.add_hotkey("d", main.write_char, args=["d"], suppress=True)
+    key.add_hotkey("f", main.write_char, args=["f"], suppress=True)
+    key.add_hotkey("g", main.write_char, args=["g"], suppress=True)
+    key.add_hotkey("h", main.write_char, args=["h"], suppress=True)
+    key.add_hotkey("j", main.write_char, args=["j"], suppress=True)
+    key.add_hotkey("k", main.write_char, args=["k"], suppress=True)
+    key.add_hotkey("l", main.write_char, args=["l"], suppress=True)
+    key.add_hotkey("z", main.write_char, args=["z"], suppress=True)
+    key.add_hotkey("x", main.write_char, args=["x"], suppress=True)
+    key.add_hotkey("c", main.write_char, args=["c"], suppress=True)
+    key.add_hotkey("v", main.write_char, args=["v"], suppress=True)
+    key.add_hotkey("b", main.write_char, args=["b"], suppress=True)
+    key.add_hotkey("n", main.write_char, args=["n"], suppress=True)
+    key.add_hotkey("m", main.write_char, args=["m"], suppress=True)
 
 
     key.wait("q+right_shift")
